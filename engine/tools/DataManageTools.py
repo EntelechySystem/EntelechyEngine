13928d5eb29744e10c682d335b0dba2a2739545f
Yee Cube <EthanSystem@outlook.com>
2024-11-05 16:09:29 +0800

    mod 从 Python 文件读取配置信息导出 Excel 文件

--- engine/tools/DataManageTools.py
+++ engine/tools/DataManageTools.py
@@ -483,4 +483,70 @@ class DataManageTools:
         return result_dict
         pass  # function
 
+    @classmethod
+    def load_configs_from_python_file_then_save_as_excel_file(cls, filepath_python: Path, filepath_excel: Path = None):
+        """
+        从 Python 文件中读取配置项和配置值，然后返回一个字典，根据配置项和配置值生成一个 Excel 文件。
+
+        Args:
+            filepath_python (Path): Python 文件路径。
+            filepath_excel (Path): 导出的 Excel 文件的路径。默认相同于 Python 路径，文件名为 config.xlsx 。
+
+        Returns:
+            result_dict (dict): 一个字典，其中配置项是键，配置值是值。
+        """
+
+        # 从路径动态导入一个 Python 文件，用到 Python 的反射功能
+        config_module = importlib.import_module(filepath_python.stem)
+        # 从 Python 文件中导入一个字典变量
+        config_dict = config_module.config
+
+        # 转换成 Pandas DataFrame
+        pd.DataFrame(config_dict.items(), columns=['配置项', '配置值']).to_excel(filepath_excel if filepath_excel else filepath_python.with_suffix('.xlsx'), index=False)
+
+        with open(filepath_python, 'r') as f:
+            lines = f.readlines()
+
+        # 正则表达式匹配键值对和注释
+        pattern = re.compile(r"\s*(\w+)\s*=\s*(.+),\s*#\s*数据类型：(.*)；配置类别：(.*)；备注：(.*)；")
+
+        # 创建DataFrame
+        keys = []
+        values = []
+        data_types = []
+        config_types = []
+        notes = []
+
+        for line in lines:
+            match = pattern.match(line)
+            if match:
+                key, value, data_type, config_type, note = match.groups()
+                keys.append(key)
+                values.append(value)
+                data_types.append(data_type.strip())
+                config_types.append(config_type.strip())
+                notes.append(note.strip())
+
+        df_config = pd.DataFrame({
+            '配置项': keys,
+            '配置值': values,
+            '数据类型': data_types,
+            '配置类别': config_types,
+            '备注': notes
+        })
+
+        # 保存为Excel文件
+        filepath_excel = filepath_excel if filepath_excel else filepath_python.with_suffix('.xlsx')
+        # Excel 文件名加一个自定义后缀
+        filepath_excel = filepath_excel.with_name(filepath_excel.stem + '_content' + filepath_excel.suffix)
+        df_config.to_excel(filepath_excel, index=False)
+
+        return config_dict
+        pass  # function
+
+        # # 示例用法
+        # filepath_python = Path('config.py')
+        # filepath_excel = Path('config.xlsx')
+        # DataManageTools.load_configs_from_python_file_then_save_as_excel_file(filepath_python, filepath_excel)
+
     pass  # class
